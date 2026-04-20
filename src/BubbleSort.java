c2047f0aa39ab19329af5db7e89dbe5119511f59
WilliamBerrezueta <lolkio079@gmail.com>
2026-04-20 08:49:30 -0500

    primer comit

---
+++ src/BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+    // constructor
+    public BubbleSort() {
+        System.out.println("Se creo la clase en el constructor");
+
+    }
+
+    public void sortAscendente(int numeros[]) {
+        for (int i = 0; i < numeros.length; i++){
+            for(int j = i+1; j < numeros.length; j++){
+                // comparacion
+                if(numeros[i] > numeros[j]){
+                    // si cumple -> intercambio
+                    int aux = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = aux;
+                }
+                }
+            }
+        }
+
+    public void printArreglo(int[] numeros) {
+      for (int i : numeros) {
+        System.out.print(i + " , ");
+      }
+      System.out.println();
+}
+
+}
